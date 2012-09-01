233b5cf6677f83456031d9b926dad0248a0ddb8b
styts <github@styts.com>
2012-09-01 21:08:08 +0200

    android fixes

--- app.py
+++ app.py
@@ -4,27 +4,48 @@ import os
 from resman import ResourceManager
 from audman import AudioManager
 
+on_android = False
+try:
+    import android
+    on_android = True
+except ImportError:
+    pass
+
 
 class App():
-    def __init__(self, title, resolution=(1024, 768), appstates=[], fps=30):
-        self.android = None
-        try:
-            import android
-            self.android = True
-        except ImportError:
-            self.android = None
+    FULLSCREEN = True
+
+    def __init__(self, title, resolutions=[(1024, 768), (960, 640), (854, 480), (800, 480), (640, 480), (480, 320)], appstates=[], fps=30):
+        global on_android
 
         os.environ['SDL_VIDEO_CENTERED'] = '1'
 
         pygame.init()
-        self.screen_w, self.screen_h = resolution
-        pygame.display.set_mode((self.screen_w, self.screen_h))
+
+        vidinfo = pygame.display.Info()
+
+        for r in resolutions:
+            if r[0] <= vidinfo.current_w and r[1] <= vidinfo.current_h:
+                break
+
+        self.screen_w, self.screen_h = r
+
+        if App.FULLSCREEN:
+            pygame.display.set_mode((vidinfo.current_w, vidinfo.current_h),  pygame.FULLSCREEN)
+            self.fullscreen = pygame.display.get_surface()
+            self.screen = pygame.Surface((self.screen_w, self.screen_h))
+            self.x = 0
+            self.y = 0
+        else:
+            pygame.display.set_mode((self.screen_w, self.screen_h))
+            self.screen = pygame.display.get_surface()
+
         pygame.display.set_caption(title)
-        self.screen = pygame.display.get_surface()
+
         self.is_running = True  # used to trigger exit by ESC
 
         # Map the back button to the escape key.
-        if self.android:
+        if on_android:
             android.init()
             android.map_key(android.KEYCODE_BACK, pygame.K_ESCAPE)
 
@@ -63,7 +84,13 @@ class App():
             self._dirty_rects.append(r)
 
     def process(self):
+        global on_android
         self.clock.tick(self._fps)
+
+        # Android-specific:
+        if on_android:
+            if android.check_pause():
+                android.wait_for_resume()
 
         self._dirty_rects = []
 
@@ -83,6 +110,11 @@ class App():
 
         events = pygame.event.get()
         for event in events:
+            try:  # hack to enable fullscreen coordinates
+                pos_x, pos_y = event.pos
+                event.pos = tuple([pos_x - self.x, pos_y - self.y])
+            except:
+                print "silly event:", event
             p = self.appstate.process_input(event)
 
             # ESC quits app
@@ -98,5 +130,11 @@ class App():
                 False, (255, 255, 255), (0, 0, 0))
             self.dirty(self.screen.blit(fps_surf, (0, 0)))
 
+        #pygame.display.update(self._dirty_rects)
+        self.x = self.fullscreen.get_width() / 2 - self.screen.get_width() / 2
+        self.y = self.fullscreen.get_height() / 2 - self.screen.get_height() / 2
+        self.fullscreen.blit(self.screen, (self.x, self.y))
+        #pygame.display.flip()
         pygame.display.update(self._dirty_rects)
+        #print self._dirty_rects
         pygame.event.pump()
